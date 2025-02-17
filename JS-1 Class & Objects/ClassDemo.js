2d22a53350813046f7e6dda52eeebabd35b85911
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2025-02-17 13:17:07 +0530

    Created the First Class in JS with member functions

---
+++ JS-1 Class & Objects/ClassDemo.js
@@ -0,0 +1,25 @@
+class Product {
+
+    name;
+    price;
+    category;
+    description;
+    rating;
+    
+
+    // These are the Member Functions
+    addToCart(){
+        console.log("Product added to cart");
+    }
+
+    removeFromCart(){
+        console.log("Product removed from cart");
+    }
+
+    displayCart(){
+        console.log("Displaying cart");
+    }
+
+    buyPro
+}
+
